4288bc486fab658fedb47030f7b81e26690a089c
Adrian Piwin <adrianpiwin@Adrians-MacBook-Pro.local>
2024-11-10 11:33:13 -0500

    Change switch to map in widget builder service

--- lib/services/health_widget_builder_service.dart
+++ lib/services/health_widget_builder_service.dart
@@ -8,68 +8,77 @@ import '../models/health_data.model.dart';
 import '../pages/home/basic_large_widget_item.dart';
 import '../pages/home/basic_widget_item.dart';
 
+typedef HealthConfigGenerator = HealthWidgetConfig Function(Goal goals, HealthData healthData);
+
 class HealthWidgetBuilderService {
   BuildContext? context;
 
-  HealthWidgetConfig generateWidgetConfig(Goal goals, HealthData healthData, HealthItem healthItemType){
-    switch(healthItemType){
-      case HealthItem.proteinIntake:
-        return HealthWidgetConfig(
-          title: "Protein",
-          subtitle: "${(goals.proteinGoal.toDouble() - healthData.getProtein).toStringAsFixed(0)} left",
-          displayValue: "${healthData.getProtein.toStringAsFixed(0)}g",
-          icon: IconTypes.proteinIcon,
-          color: RepresentationColors.proteinColor,
-          size: 2,
-          goalPercent: healthData.getProtein / goals.proteinGoal.toDouble(),
-        );
-      case HealthItem.exerciseTime:
-        return HealthWidgetConfig(
-          title: "Exercise Time",
-          subtitle: "${(goals.exerciseMinutesGoal.toDouble() - healthData.getExerciseMinutes).toStringAsFixed(0)} left",
-          displayValue: "${healthData.getExerciseMinutes.toStringAsFixed(0)}min",
-          icon: IconTypes.exerciseIcon,
-          color: RepresentationColors.exerciseColor,
-          size: 2,
-          goalPercent: healthData.getExerciseMinutes / goals.exerciseMinutesGoal.toDouble(),
-        );
-      case HealthItem.sleepDuration:
-        int totalMinutes = healthData.getSleep.toInt();
-        int hours = totalMinutes ~/ 60;
-        int minutes = totalMinutes % 60;
-        String displayValue = "$hours:${minutes.toString().padLeft(2, '0')} hrs";
+  final Map<HealthItem, HealthConfigGenerator> _healthConfigGenerators = {
+    HealthItem.proteinIntake: (Goal goals, HealthData healthData) {
+      return HealthWidgetConfig(
+        title: "Protein",
+        subtitle: "${(goals.proteinGoal.toDouble() - healthData.getProtein).toStringAsFixed(0)}g left",
+        displayValue: "${healthData.getProtein.toStringAsFixed(0)}g",
+        icon: IconTypes.proteinIcon,
+        color: RepresentationColors.proteinColor,
+        size: 2,
+        goalPercent: healthData.getProtein / goals.proteinGoal.toDouble(),
+      );
+    },
+    HealthItem.exerciseTime: (Goal goals, HealthData healthData) {
+      return HealthWidgetConfig(
+        title: "Exercise Time",
+        subtitle: "${(goals.exerciseMinutesGoal.toDouble() - healthData.getExerciseMinutes).toStringAsFixed(0)}min left",
+        displayValue: "${healthData.getExerciseMinutes.toStringAsFixed(0)}min",
+        icon: IconTypes.exerciseIcon,
+        color: RepresentationColors.exerciseColor,
+        size: 2,
+        goalPercent: healthData.getExerciseMinutes / goals.exerciseMinutesGoal.toDouble(),
+      );
+    },
+    HealthItem.sleepDuration: (Goal goals, HealthData healthData) {
+      int totalMinutes = healthData.getSleep.toInt();
+      int hours = totalMinutes ~/ 60;
+      int minutes = totalMinutes % 60;
+      String displayValue = "$hours:${minutes.toString().padLeft(2, '0')}hrs";
 
-        int sleepGoalMinutes = goals.sleepGoal.inMinutes;
-        int actualSleepMinutes = healthData.getSleep.toInt();
-        int differenceMinutes = (sleepGoalMinutes - actualSleepMinutes).abs();
+      int sleepGoalMinutes = goals.sleepGoal.inMinutes;
+      int actualSleepMinutes = healthData.getSleep.toInt();
+      int differenceMinutes = (sleepGoalMinutes - actualSleepMinutes).abs();
 
-        String displayDifference;
-        if (differenceMinutes >= 60) {
-          int hours = differenceMinutes ~/ 60;
-          int minutes = differenceMinutes % 60;
-          displayDifference = "$hours:${minutes.toString().padLeft(2, '0')} hrs from goal";
-        } else {
-          displayDifference = "$differenceMinutes min from goal";
-        }
-        return HealthWidgetConfig(
-          title: "Sleep Duration",
-          subtitle: displayDifference,
-          displayValue: displayValue,
-          icon: IconTypes.sleepDurationIcon,
-          color: RepresentationColors.sleepColor,
-          size: 2,
-          goalPercent: healthData.getSleep / goals.sleepGoal.inMinutes.toDouble(),
-        );
-      default:
-        return HealthWidgetConfig(
-          title: "Incorrect type",
-          subtitle: "",
-          displayValue: "",
-          icon: Icons.error,
-          color: Colors.red,
-          size: 1,
-          goalPercent: 0,
-        );
+      String displayDifference;
+      if (differenceMinutes >= 60) {
+        int hours = differenceMinutes ~/ 60;
+        int minutes = differenceMinutes % 60;
+        displayDifference = "$hours:${minutes.toString().padLeft(2, '0')}hrs from goal";
+      } else {
+        displayDifference = "$differenceMinutes min from goal";
+      }
+      return HealthWidgetConfig(
+        title: "Sleep Duration",
+        subtitle: displayDifference,
+        displayValue: displayValue,
+        icon: IconTypes.sleepDurationIcon,
+        color: RepresentationColors.sleepColor,
+        size: 2,
+        goalPercent: healthData.getSleep / goals.sleepGoal.inMinutes.toDouble(),
+      );
+    }
+  };
+
+  HealthWidgetConfig generateWidgetConfig(Goal goals, HealthData healthData, HealthItem healthItemType) {
+    if (_healthConfigGenerators.containsKey(healthItemType)) {
+      return _healthConfigGenerators[healthItemType]!(goals, healthData);
+    } else {
+      return HealthWidgetConfig(
+        title: "Incorrect type",
+        subtitle: "",
+        displayValue: "",
+        icon: Icons.error,
+        color: Colors.red,
+        size: 1,
+        goalPercent: 0,
+      );
     }
   }
 
@@ -78,13 +87,13 @@ class HealthWidgetBuilderService {
 
     return {
       "size": config.size,
-      "widget": config.size == 1 ?
-        BasicWidgetItem(
-          config: config,
-        ) :
-        BasicLargeWidgetItem(
-          config: config,
-        )
+      "widget": config.size == 1
+          ? BasicWidgetItem(
+              config: config,
+            )
+          : BasicLargeWidgetItem(
+              config: config,
+            )
     };
   }
 }
