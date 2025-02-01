3b5fedd73621061306eb41ea40d1baca12390730
Adrian Piwin <adrianpiwin@Adrians-MacBook-Pro.local>
2025-02-01 13:28:07 -0500

    Include today on streak

--- lib/services/streak_service.dart
+++ lib/services/streak_service.dart
@@ -3,8 +3,9 @@ import 'package:healthxp/models/health_entities/health_entity.model.dart';
 
 class StreakService {
   Future<int> getStreak(HealthEntity entity, double goalValue) async {
-    DateTime currentDate = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day).subtract(const Duration(days: 1));
+    DateTime currentDate = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
     int streak = 0;
+    bool isFirstDay = true;
 
     while (true) {
       final dayData = await entity.getData(DateTimeRange(
@@ -12,7 +13,7 @@ class StreakService {
         end: currentDate.add(const Duration(days: 1)),
       ));
       
-      if (dayData.isEmpty) break;
+      if (dayData.isEmpty && !isFirstDay) break;
       
       double dailyTotal = entity.aggregateData(dayData).fold(
         0.0, 
@@ -21,10 +22,11 @@ class StreakService {
       
       if (dailyTotal.abs() >= goalValue.abs()) {
         streak++;
-      } else {
+      } else if (!isFirstDay) {
         break;
       }
       
+      isFirstDay = false;
       currentDate = currentDate.subtract(const Duration(days: 1));
     }
     
